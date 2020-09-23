1bcada3692af16ba647e0213f9ac752eb542e824
Lucie Wang <lucie.wangxy@gmail.com>
2020-09-22 21:58:51 -0700

    config updates

--- src/main/java/goCart/service/CustomerService.java
+++ src/main/java/goCart/service/CustomerService.java
@@ -1,2 +1,31 @@
-package goCart.service;public class CustomerService {
+package goCart.service;
+
+import goCart.model.Cart;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import goCart.dao.CustomerDao;
+import goCart.model.Customer;
+
+@Service
+public class CustomerService {
+
+    @Autowired
+    private CustomerDao customerDao;
+
+    public void addCustomer(Customer customer) {
+        customer.getUser().setEnabled(true);
+
+        Cart cart = new Cart();
+        customer.setCart(cart);
+
+        customerDao.addCustomer(customer);
+    }
+
+    public Customer getCustomerByUserName(String userName) {
+        return customerDao.getCustomerByUserName(userName);
+    }
 }
+
+
+
